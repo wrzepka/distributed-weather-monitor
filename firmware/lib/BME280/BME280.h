39882bd5a0d7940dbed5d34f60040f5c434828d4
wrzepka <90323901+wrzepka@users.noreply.github.com>
2026-07-21 14:24:57 +0200

    docs(BME280): complete missing part of the documentation

--- firmware/lib/BME280/BME280.h
+++ firmware/lib/BME280/BME280.h
@@ -27,37 +27,40 @@
  */
 class BME280 {
 public:
-    static constexpr uint8_t DEFAULT_I2C_ADDR = 0x76;
-    static constexpr uint8_t SECONDARY_I2C_ADDR = 0x77;
+    static constexpr uint8_t DEFAULT_I2C_ADDR = 0x76 /**< Default I2C address for BME280 sensor.*/;
+    static constexpr uint8_t SECONDARY_I2C_ADDR = 0x77; /**< Secondary I2C address for BME280 sensor.*/;
 
     /**
      * @brief Contains BME280 calibration data for temperature, pressure and humidity.
      */
     struct bme280_calib_data {
-        uint16_t dig_T1; /**<calibration T1 data*/
-        int16_t dig_T2; /**<calibration T2 data*/
-        int16_t dig_T3; /**<calibration T3 data*/
+        uint16_t dig_T1; /**<calibration T1 data.*/
+        int16_t dig_T2; /**<calibration T2 data.*/
+        int16_t dig_T3; /**<calibration T3 data.*/
 
-        uint16_t dig_P1; /**<calibration P1 data*/
-        int16_t dig_P2; /**<calibration P2 data*/
-        int16_t dig_P3; /**<calibration P3 data*/
-        int16_t dig_P4; /**<calibration P4 data*/
-        int16_t dig_P5; /**<calibration P5 data*/
-        int16_t dig_P6; /**<calibration P6 data*/
-        int16_t dig_P7; /**<calibration P7 data*/
-        int16_t dig_P8; /**<calibration P8 data*/
-        int16_t dig_P9; /**<calibration P9 data*/
+        uint16_t dig_P1; /**<calibration P1 data.*/
+        int16_t dig_P2; /**<calibration P2 data.*/
+        int16_t dig_P3; /**<calibration P3 data.*/
+        int16_t dig_P4; /**<calibration P4 data.*/
+        int16_t dig_P5; /**<calibration P5 data.*/
+        int16_t dig_P6; /**<calibration P6 data.*/
+        int16_t dig_P7; /**<calibration P7 data.*/
+        int16_t dig_P8; /**<calibration P8 data.*/
+        int16_t dig_P9; /**<calibration P9 data.*/
 
-        uint8_t dig_H1; /**<calibration H1 data*/
-        int16_t dig_H2; /**<calibration H2 data*/
-        uint8_t dig_H3; /**<calibration H3 data*/
-        int16_t dig_H4; /**<calibration H4 data*/
-        int16_t dig_H5; /**<calibration H5 data*/
-        int8_t dig_H6; /**<calibration H6 data*/
+        uint8_t dig_H1; /**<calibration H1 data.*/
+        int16_t dig_H2; /**<calibration H2 data.*/
+        uint8_t dig_H3; /**<calibration H3 data.*/
+        int16_t dig_H4; /**<calibration H4 data.*/
+        int16_t dig_H5; /**<calibration H5 data.*/
+        int8_t dig_H6; /**<calibration H6 data.*/
 
-        int32_t t_fine; /**<calibration T_FINE data*/
+        int32_t t_fine; /**<calibration T_FINE data.*/
     };
 
+    /**
+     * @brief BME280 oversampling setting.
+     */
     enum class Oversampling : uint8_t {
         Skipped = 0x00,
         X1 = 0x01,
@@ -67,12 +70,18 @@ public:
         X16 = 0x05,
     };
 
+    /**
+     * @brief BME280 mode of work.
+     */
     enum class Mode : uint8_t {
         Sleep = 0x00,
         Forced = 0x01,
         Normal = 0x03
     };
 
+    /**
+     * @brief Config structure for easier ctrl_hum and ctrl_meas register configuration.
+     */
     struct Config {
         Oversampling temp_oversampling = Oversampling::X1;
         Oversampling press_oversampling = Oversampling::X1;
@@ -86,17 +95,26 @@ public:
     /**
      * @brief Initializes I2C connection for BME280 sensor.
      *
-     * Configures device handler, add it to I2C bus. Next set up ctrl_hum and ctrl_meas registers with sleep mode and x1
-     * oversampling.
+     * Configures device handler, add it to I2C bus. Next initializes apply_config() and read_calib_data().
      *
      * @param bus_handle I2C master bus handle.
-     * @param config
-     * @return false when initialization failed, otherwise true.
+     * @param config BME280 configuration structure.
+     * @return ESP_ERR_INVALID_ARG if bus handle is nullptr.
+     * @return ESP_OK if initialization succeed.
+     * @return Other esp errors if something went wrong.
      */
     esp_err_t begin(i2c_master_bus_handle_t bus_handle, const Config &config);
 
+    /**
+     * @brief Apply Config structure.
+     *
+     * Uses Config structure to setups ctrl_hum and ctrl_meas registers.
+     *
+     * @param config BME280 configuration structure.
+     * @return ESP_OK if configuration setup succeed.
+     * @return Other esp errors if something went wrong.
+     */
     esp_err_t apply_config(const Config &config);
-
 
     /**
      * @brief Reads calibration data from BME280 registers.
@@ -109,12 +127,13 @@ public:
 
     /**
      * @brief Gives access to private _calib_data field.
-     * @return static reference to _calib_data field.
+    * @return ESP_OK if configuration setup succeed.
+     * @return Other esp errors if something went wrong.
      */
     const bme280_calib_data &calib_data() const;
 
     /**
-     * @brief
+     * @brief Prints calibration parameters.
      */
     void print_calib_data() const;
 
@@ -124,7 +143,7 @@ public:
      * Calculates compensated value based on 32bit formula from datasheet.
      *
      * @param adc_temp ADC temperature value read from sensor registers.
-     * @return Compensated value in integer format (ex. 2560 - 25,60*C)
+     * @return Compensated value in integer format (ex. 2560 - 25,60*C).
      */
     int32_t compensate_temperature(int32_t adc_temp);
 
@@ -134,7 +153,7 @@ public:
      * Calculates compensated value based on 32bit formula from datasheet.
      *
      * @param adc_press ADC pressure value read from sensor registers.
-     * @return Compensated value in unsigned integer format (ex. 96368 - 963.68hPa)
+     * @return Compensated value in unsigned integer format (ex. 96368 - 963.68hPa).
      */
     uint32_t compensate_pressure(int32_t adc_press);
 
@@ -144,7 +163,7 @@ public:
      * Calculates compensated value based on 32bit formula from datasheet.
      *
      * @param adc_H ADC humidity value read from sensor registers.
-     * @return Compensated value in unsigned integer format (ex. 47445 - 46.333 %RH (after dividing by 1024!))
+     * @return Compensated value in unsigned integer format (ex. 47445 - 46.333 %RH (after dividing by 1024!)).
      */
     uint32_t compensate_humidity(int32_t adc_H);
 
@@ -159,20 +178,20 @@ public:
     esp_err_t read_weather_data();
 
 private:
-    uint8_t _address; /**< Device I2C address (default: 0x76 or 0x77 if specified pin is high) */
-    i2c_master_dev_handle_t _dev_handle; /**< I2C device handle */
-    bme280_calib_data _calib_data; /**< Structure for calibration data*/
-    int32_t fine_temp; /**< Value from compensate_temperature() used in other compensational methods*/
-    static constexpr uint8_t REG_CALIB_00_ADDR = 0x88;
-    static constexpr uint8_t REG_CALIB_26_ADDR = 0xE1;
-    static constexpr uint8_t REG_CONTROL_HUM_ADDR = 0xF2;
-    static constexpr uint8_t REG_CONTROL_MEAS_ADDR = 0xF4;
-    static constexpr uint8_t REG_MEAS_DATA_START_ADDR = 0xF7;
-    static constexpr uint8_t CALIB_00_PAYLOAD_SIZE = 26;
-    static constexpr uint8_t CALIB_26_PAYLOAD_SIZE = 7;
-    static constexpr uint8_t MEAS_DATA_PAYLOAD_SIZE = 8;
-    static constexpr uint32_t MAX_RESPONSE_TIME_IN_MS = 100;
-    static constexpr uint32_t SUITABLE_MEASUREMENT_DELAY_IN_MS = 100;
+    uint8_t _address; /**< Device I2C address (default: 0x76 or 0x77 if specified pin is high).*/
+    i2c_master_dev_handle_t _dev_handle; /**< I2C device handle.*/
+    bme280_calib_data _calib_data; /**< Structure for calibration data.*/
+    int32_t fine_temp; /**< Value from compensate_temperature() used in other compensational methods.*/
+    static constexpr uint8_t REG_CALIB_00_ADDR = 0x88; /** Address of the first part of calib data register.*/
+    static constexpr uint8_t REG_CALIB_26_ADDR = 0xE1; /** Address of the second part of calib data register.*/
+    static constexpr uint8_t REG_CONTROL_HUM_ADDR = 0xF2; /** ctrl_hum register address.*/
+    static constexpr uint8_t REG_CONTROL_MEAS_ADDR = 0xF4; /** ctrl_meas register address.*/
+    static constexpr uint8_t REG_MEAS_DATA_START_ADDR = 0xF7; /** Address of the start of the measurement data payload.*/
+    static constexpr uint8_t CALIB_00_PAYLOAD_SIZE = 26; /** Size of the first part of calib data payload.*/
+    static constexpr uint8_t CALIB_26_PAYLOAD_SIZE = 7; /** Size of the second part of calib data payload.*/
+    static constexpr uint8_t MEAS_DATA_PAYLOAD_SIZE = 8; /** Size of the measurement data payload.*/
+    static constexpr uint32_t MAX_RESPONSE_TIME_IN_MS = 100; /** Maximum waiting time for response in ms.*/
+    static constexpr uint32_t SUITABLE_MEASUREMENT_DELAY_IN_MS = 50; /** Appropriate delay for proper measurement.*/
 };
 
 #endif //FIRMWARE_BME280_H
