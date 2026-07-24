4a648304d392d708d95fcbf9ee29c55a78a838af
wrzepka <90323901+wrzepka@users.noreply.github.com>
2026-07-24 18:37:51 +0200

    feat(mqtt): finish basic connect() method

--- firmware/src/MqttTransport.cpp
+++ firmware/src/MqttTransport.cpp
@@ -7,7 +7,49 @@
 
 
 esp_err_t MqttTransport::connect() {
-    return ESP_OK;
+    esp_mqtt_client_config_t mqtt_cfg = {};
+    mqtt_cfg.broker.address.hostname = this->m_brokerIp;
+    mqtt_cfg.broker.address.port = this->m_brokerPort;
+    mqtt_cfg.session.protocol_ver = MQTT_PROTOCOL_V_5;
+    mqtt_cfg.session.disable_clean_session = false;
+
+    esp_mqtt5_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    if (client == nullptr) {
+        return ESP_FAIL;
+    }
+    this->m_client = client;
+
+    esp_err_t result = ESP_OK;
+    result = esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt5_event_handler, this);
+    if (result != ESP_OK) {
+        return result;
+    }
+    result = esp_mqtt_client_start(client);
+    //TODO: memory leak fix.
+    //TODO: async control (semaphor?)
+    return result;
+}
+
+void MqttTransport::mqtt5_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data) {
+    auto mqtt_client = static_cast<MqttTransport *>(handler_args);
+    esp_mqtt_event_id_t event = static_cast<esp_mqtt_event_id_t>(event_id);
+
+    switch (event) {
+        case MQTT_EVENT_CONNECTED:
+            //TODO: Implement
+            break;
+        case MQTT_EVENT_PUBLISHED:
+            //TODO: Implement
+            break;
+        case MQTT_EVENT_DISCONNECTED:
+            //TODO: Implement
+            break;
+        case MQTT_EVENT_ERROR:
+            //TODO: Implement
+            break;
+        default:
+            break;
+    }
 }
 
 esp_err_t MqttTransport::disconnect() {
--- firmware/src/MqttTransport.h
+++ firmware/src/MqttTransport.h
@@ -4,6 +4,8 @@
 
 #ifndef FIRMWARE_MQTTTRANSPORT_H
 #define FIRMWARE_MQTTTRANSPORT_H
+#include <esp_event_base.h>
+
 #include "ITelemetryTransport.h"
 
 
@@ -11,9 +13,12 @@ class MqttTransport : public ITelemetryTransport {
 private:
     const char *m_brokerIp;
     uint16_t m_brokerPort;
+    esp_mqtt_client_handle_t m_client;
+
+    static void mqtt5_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data);
 
 public:
-    MqttTransport(const char *brokerIp, uint16_t brokerPort) : m_brokerIp(brokerIp), m_brokerPort(brokerPort) {
+    MqttTransport(const char *brokerIp, uint16_t brokerPort) : m_brokerIp(brokerIp), m_brokerPort(brokerPort), m_client(nullptr) {
     };
 
     esp_err_t connect() override;
